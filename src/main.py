52d67fd256778f83bcbc913c159fe27ab8ce6d9c
AndyC5H10O5 <1915965336@qq.com>
2026-01-05 18:02:21 +0800

    成功调用tool（并不稳定）

--- src/main.py
+++ src/main.py
@@ -1,22 +1,38 @@
 import os
 from langchain.agents import create_agent
-from src.tools.get import get_Chinese_8word
+from langchain.chat_models import init_chat_model
+
+# from src.tools.get import get_Chinese_8word
 
 os.environ["DEEPSEEK_API_KEY"] = "sk-3a740dbac47d4f599c2708ee0b52fb76"
 
-agent = create_agent(
-    model="deepseek-reasoner",
-    tools=[get_Chinese_8word],
-    system_prompt="你是一个起名师傅，擅长给中国宝宝古法取名"
+model = init_chat_model(
+    "deepseek-chat",
 )
 
-agent.invoke(
+system_prompt = "你是一个起名师傅，擅长给中国宝宝古法取名"
+
+
+def get_Chinese_8word() -> str:
+    """获得生辰八字"""
+    return "甲申 丙子 庚辰 甲申"
+
+
+agent = create_agent(
+    model=model,
+    tools=[get_Chinese_8word],
+    system_prompt=system_prompt
+)
+
+response = agent.invoke(
     {
         "message": [
             {
                 "role": "user",
-                "content": "我的生辰八字是什么"
+                "content": "你的角色是什么"
             }
         ]
     }
 )
+
+print(response)
